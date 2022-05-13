abe93ae746a13f036263881a17d49a06a87cbbaf
Chanran33 <k55416325@gmail.com>
2022-05-14 01:39:23 +0900

    필터링 기능 + memo hook

--- dsfes_front/src/App.js
+++ dsfes_front/src/App.js
@@ -7,6 +7,9 @@ import Board from "./pages/Board";
 import NoticeManage from "./pages/NoticeManage";
 import { Routes, Route } from "react-router-dom";
 import AdminNotice from "./pages/AdminNotice";
+import NoticeListV1 from "./components/NoticeListV1";
+import NoticeListV2 from "./components/NoticeListV2";
+import NoticeListV3 from "./components/NoticeListV3";
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
       <Route path="/" element={<Menubar />}>
         <Route index element={<Main />}></Route>
         <Route path="/Notice" element={<Notice />}></Route>
+        <Route path="/Notice1" element={<NoticeListV1 />}></Route>
+        <Route path="/Notice2" element={<NoticeListV2 />}></Route>
+        <Route path="/Notice3" element={<NoticeListV3 />}></Route>
         <Route path="/timetable" element={<TimeTable />}></Route>
         <Route path="/event" element={<Event />}></Route>
         <Route path="/board" element={<Board />}></Route>
